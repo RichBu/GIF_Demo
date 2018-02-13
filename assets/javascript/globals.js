3e9dee579bed3bbcf2c477d9a21c5c26508286b6
Richard Budek <richardbudek@Richards-Air.attlocal.net>
2018-02-13 02:29:01 -0600

    obj models done
    finished obj models for button and gif

--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -29,8 +29,8 @@ var configData = {
 
 
 var topicObj = {
-    topicSubject : "Machining",
-    topicsPreset : [            //array that started with
+    topicSubject: "Machining",
+    topicsPreset: [            //array that started with
         "Endmill",      //0
         "CNC",          //1
         "CAD",          //2
@@ -43,27 +43,27 @@ var topicObj = {
         "Bandsaw cutting",  //9 
     ],
 
-    topicsWorkingArray : [],  //aray that will be working with
+    topicsWorkingArray: [],  //aray that will be working with
 
     addTopic: function (newTopic) {
-        this.topicsWorkingArray.push(newTopic);        
+        this.topicsWorkingArray.push(newTopic);
     },
 
-    initWorkingArray: function() {
+    initWorkingArray: function () {
         //loads the working aray from the preset array
         var stopVal = this.topicsPreset.length;
-        for ( var i=0; i<stopVal; i++) {
-            this.addTopic( this.topicsPreset[i]);  //push to working array
+        for (var i = 0; i < stopVal; i++) {
+            this.addTopic(this.topicsPreset[i]);  //push to working array
         };
     },
 
-    retTopicString : function( numIn )  {
+    retTopicString: function (numIn) {
         var outVal = "";
         outVal = this.topicsWorkingArray[numIn];
         return outVal;
     },
 
-    retNumWorkingTopics : function () {
+    retNumWorkingTopics: function () {
         var outVal = 0;
         outVal = this.topicsWorkingArray.length;
         return outVal;
@@ -72,40 +72,41 @@ var topicObj = {
 
 
 var singleButtonObj = {
-    topic : "",
-    urlAddr : "",   //url address to GIF
-    EOF : ""  //just place keeper to not worry about ,
+    topic: "",
+    searchTerm: "", //search term if different
+    EOF: ""  //just place keeper to not worry about ,
 };
 
 var buttonObj = {
-    buttonArray : [],  //array pf singleButtonObj
+    buttonArray: [],  //array pf singleButtonObj
 
-    addButtonToArray : function ( singleBttnObjIn ) {
-        var newObj = jQuery.extend(true, {}, singleBttnObjIn );
-        this.buttonArray.push( newObj );
+    addButtonToArray: function (singleBttnObjIn) {
+        var newObj = jQuery.extend(true, {}, singleBttnObjIn);
+        this.buttonArray.push(newObj);
     },
 
-    clearButtonArray : function () {
+    clearButtonArray: function () {
         //pop all elements from array
         var endVal = this.buttonArray.length;
-        for ( var i=0; i<endVal; i++) {
+        for (var i = 0; i < endVal; i++) {
             this.buttonArray.pop();
         };
     },
 
-    addNewButton : function ( topicIn ) {
+    addNewButton: function (topicIn) {
         //takes a topic and adds a button to page
         //assume topic in array already
         singleButtonObj.topic = topicIn;
+        singleButtonObj.searchTerm = topicIn;  //right now both the same
         this.addButtonToArray(singleButtonObj);
         var divButtons = $(configData.divTopicButtons);
         var newButton = $("<button>");
-        $(newButton).addClass( "btn");
-        $(newButton).addClass( "btn-info");
+        $(newButton).addClass("btn");
+        $(newButton).addClass("btn-info");
         $(newButton).css("margin", "5px");
-        $(newButton).text( topicIn );
-        $(newButton).attr("data-button", this.buttonArray.length-1);        
-        $(newButton).appendTo( divButtons );
+        $(newButton).text(topicIn);
+        $(newButton).attr("data-button", this.buttonArray.length - 1);
+        $(newButton).appendTo(divButtons);
     },
 
     refreshAllButtons: function () {
@@ -114,15 +115,59 @@ var buttonObj = {
         var divButtons = $(configData.divTopicButtons);
         divButtons.html("");
         var endVal = topicObj.retNumWorkingTopics();
-        for ( var i=0; i<endVal; i++) {
-            this.addNewButton( topicObj.retTopicString(i) );
+        for (var i = 0; i < endVal; i++) {
+            this.addNewButton(topicObj.retTopicString(i));
         };
     },
 
-    EOF : ""  //just place keeper to not worry about ,
+    retSearchStr: function(bttnNum) {
+        //returns the search string for a button
+        //normally just the text on the button
+        //but have made allotments to make it different
+        var outVal = "";
+        outVal = this.buttonArray[bttnNum].searchTerm;
+        return outVal;
+    },
+
+    EOF: ""  //just place keeper to not worry about ,
 };
 
+
+var gifSingleObj = {
+    status: "",     //
+    urlStill: "",
+    urlAnimated: "",
+    topic: "",         //topic associated with
+    searchString: "",  //search string used to search this
+    EOF: "" //just place keeper
+};
+
+
 var GIFobj = {
+    //everything associated with all the GIF's
+    GIFarray: [],  //array of gifSingleObj
+
+    clearGIFdisplay: function () {
+        var divGIF = $(configData.divGIF);
+        divGIF.html("");
+    },
+
+    clearEverything: function () {
+        //clear out all the GIF's on the page
+        this.clearGIFdisplay();
+
+        //now clear out the array
+        var endVal = GIFarray.length;
+        for ( var i=0; i<endVal; i++)
+        {
+            this.GIFarray.pop();
+        }
+    },
+    EOF: ""  //place keeper
+};
+
+
+var searchParmObj = {
 
 };
 
